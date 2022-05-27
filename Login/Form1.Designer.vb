3c4ccd66e747c7c622f3122f83fd2e78d11b1486
Shwe Eain San <Shwe Eain San@DESKTOP-73GSVF4>
2022-05-27 11:18:59 +0630

    by ses

---
+++ Login/Form1.Designer.vb
@@ -0,0 +1,134 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Login
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtPassword = New System.Windows.Forms.TextBox()
+        Me.txtName = New System.Windows.Forms.TextBox()
+        Me.pictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        CType(Me.pictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'txtPassword
+        '
+        Me.txtPassword.Font = New System.Drawing.Font("MS Gothic", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtPassword.Location = New System.Drawing.Point(352, 206)
+        Me.txtPassword.MaxLength = 10
+        Me.txtPassword.Name = "txtPassword"
+        Me.txtPassword.PasswordChar = Global.Microsoft.VisualBasic.ChrW(42)
+        Me.txtPassword.Size = New System.Drawing.Size(300, 26)
+        Me.txtPassword.TabIndex = 38
+        '
+        'txtName
+        '
+        Me.txtName.Font = New System.Drawing.Font("MS Gothic", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtName.Location = New System.Drawing.Point(352, 146)
+        Me.txtName.MaxLength = 10
+        Me.txtName.Name = "txtName"
+        Me.txtName.Size = New System.Drawing.Size(300, 26)
+        Me.txtName.TabIndex = 37
+        '
+        'pictureBox1
+        '
+        Me.pictureBox1.Image = Global.Login.My.Resources.Resources.C_Logo
+        Me.pictureBox1.Location = New System.Drawing.Point(354, 17)
+        Me.pictureBox1.Name = "pictureBox1"
+        Me.pictureBox1.Size = New System.Drawing.Size(271, 73)
+        Me.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.pictureBox1.TabIndex = 41
+        Me.pictureBox1.TabStop = False
+        '
+        'Button2
+        '
+        Me.Button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button2.Font = New System.Drawing.Font("MS Gothic", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button2.Location = New System.Drawing.Point(698, 468)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(287, 64)
+        Me.Button2.TabIndex = 39
+        Me.Button2.Text = "F12"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button1
+        '
+        Me.Button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button1.Font = New System.Drawing.Font("MS Gothic", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.Location = New System.Drawing.Point(2, 468)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(287, 64)
+        Me.Button1.TabIndex = 40
+        Me.Button1.Text = "F1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("MS Gothic", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(252, 209)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(97, 19)
+        Me.Label2.TabIndex = 36
+        Me.Label2.Text = "Password"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("MS Gothic", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(241, 149)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(108, 19)
+        Me.Label1.TabIndex = 35
+        Me.Label1.Text = "User Name"
+        '
+        'Login
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(987, 534)
+        Me.Controls.Add(Me.txtPassword)
+        Me.Controls.Add(Me.txtName)
+        Me.Controls.Add(Me.pictureBox1)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Login"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Login"
+        CType(Me.pictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents txtPassword As TextBox
+    Friend WithEvents txtName As TextBox
+    Private WithEvents pictureBox1 As PictureBox
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+End Class
---
+++ Login/Form1.vb
@@ -0,0 +1,50 @@
+﻿Imports System
+Imports System.Data.SqlClient
+Public Class Login
+    Dim username As String
+    Dim password As String
+    Dim connetionString As String
+    Dim Connection As SqlConnection
+    Dim cmd As SqlCommand
+    Dim adapter As SqlDataAdapter
+    Dim dt As DataTable
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Application.Exit()
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        If txtName.Text = Nothing Or txtPassword.Text = Nothing Then
+            MsgBox("Enter Credential", MsgBoxStyle.Exclamation)
+            txtName.Select()
+            Return
+        Else
+            connetionString = "Data Source=163.43.105.244;Initial Catalog=Accele_Test;User ID=sa;Password=admin123456!;Connect Timeout=30"
+            Connection = New SqlConnection(connetionString)
+            Connection.Open()
+            cmd = New SqlCommand("Select * from M_User where [UserName]='" & txtName.Text & "' And [Password]='" & txtPassword.Text & "'", Connection)
+
+            Using sda As New SqlDataAdapter(cmd)
+                Using dt As New DataTable()
+                    sda.Fill(dt)
+                    If (dt.Rows.Count > 0) Then
+                        MsgBox("Login Success", MsgBoxStyle.Information)
+                    Else
+                        MsgBox("Check UserName and Password", MsgBoxStyle.Critical)
+                        txtName.Select()
+                    End If
+                    Connection.Close()
+                End Using
+            End Using
+        End If
+    End Sub
+
+    Private Sub Ctrl_KeyUp(sender As Object, e As KeyEventArgs) Handles txtPassword.KeyUp, txtName.KeyUp
+        If e.KeyValue = Keys.Enter Then
+            Me.SelectNextControl(sender, True, True, True, True)
+        End If
+    End Sub
+
+    Private Sub Login_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        txtName.Select()
+    End Sub
+End Class
