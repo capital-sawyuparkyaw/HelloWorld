774a13c5fb3a2a152f3d5eb0f79ea5a8e510a0af
Shwe Eain San <Shwe Eain San@DESKTOP-73GSVF4>
2022-06-07 12:44:54 +0630

    by ses

---
+++ Custom_Controls/Common_Label.vb
@@ -0,0 +1,48 @@
+﻿Imports System.Windows.Forms
+Imports System.Drawing
+
+Public Class Common_Label
+    Inherits Label
+    Dim tmr As FontSize
+    Public Sub New()
+        If FontSize.Normal Then
+            Me.Font = New System.Drawing.Font("MS Gothic", 9.0F, System.Drawing.FontStyle.Bold)
+        ElseIf FontSize.Small Then
+            Me.Font = New System.Drawing.Font("MS Gothic", 14.0F, System.Drawing.FontStyle.Bold)
+        ElseIf FontSize.Medium Then
+            Me.Font = New System.Drawing.Font("MS Gothic", 16.0F, System.Drawing.FontStyle.Bold)
+        ElseIf FontSize.Large Then
+            Me.Font = New System.Drawing.Font("MS Gothic", 20.0F, System.Drawing.FontStyle.Bold)
+        ElseIf FontSize.XLarge Then
+            Me.Font = New System.Drawing.Font("MS Gothic", 24.0F, System.Drawing.FontStyle.Bold)
+        End If
+    End Sub
+
+    Public Enum Color
+        White
+        Green
+        DarkGreen
+        Blue
+    End Enum
+
+    Public Enum FontSize
+        Normal
+        Small
+        Medium
+        Large
+        XLarge
+    End Enum
+    'Private Sub Font_Size(ByVal Obj As Object, ByVal e As EventArgs)
+    '    If FontSize.Normal Then
+    '        Me.Font = New System.Drawing.Font("MS Gothic", 9.0F, System.Drawing.FontStyle.Bold)
+    '    ElseIf FontSize.Small Then
+    '        Me.Font = New System.Drawing.Font("MS Gothic", 14.0F, System.Drawing.FontStyle.Bold)
+    '    ElseIf FontSize.Medium Then
+    '        Me.Font = New System.Drawing.Font("MS Gothic", 16.0F, System.Drawing.FontStyle.Bold)
+    '    ElseIf FontSize.Large Then
+    '        Me.Font = New System.Drawing.Font("MS Gothic", 20.0F, System.Drawing.FontStyle.Bold)
+    '    ElseIf FontSize.XLarge Then
+    '        Me.Font = New System.Drawing.Font("MS Gothic", 24.0F, System.Drawing.FontStyle.Bold)
+    '    End If
+    'End Sub
+End Class
---
+++ Custom_Controls/Common_RadioButton.vb
@@ -0,0 +1,19 @@
+﻿Imports System.Windows.Forms
+Imports System.Drawing
+Public Class Common_RadioButton
+    Inherits RadioButton
+
+    Dim x As New Font("Segoe UI", 10)
+    Dim y As New SolidBrush(Color.Black)
+    Dim z As New Point(0, 0)
+    Public Property Icons As String
+    Public Common_RadioButton()
+    Dim a As New System.Drawing.Font("MS Gothic", 9.0F, System.Drawing.FontStyle.Bold)
+
+    Protected Overrides Sub OnPaint(e As PaintEventArgs)
+        MyBase.OnPaint(e)
+        e.Graphics.DrawString(Icons, x, y, z)
+        Me.Font = New System.Drawing.Font("MS Gothic", 9.0F, System.Drawing.FontStyle.Bold)
+        SetStyle(ControlStyles.UserPaint, True)
+    End Sub
+End Class
