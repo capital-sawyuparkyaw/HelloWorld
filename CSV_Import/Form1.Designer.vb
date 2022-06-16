9b2c2612f81d5d23b380f322f2dab8b1e61edceb
Shwe Eain San <Shwe Eain San@DESKTOP-73GSVF4>
2022-06-16 08:17:19 +0630

    by ses

---
+++ CSV_Import/Form1.Designer.vb
@@ -0,0 +1,132 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class CSV_Import
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.dataGridview1 = New System.Windows.Forms.DataGridView()
+        Me.btnImport = New System.Windows.Forms.Button()
+        Me.txtpathfile = New System.Windows.Forms.TextBox()
+        Me.Column1 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column2 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column3 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column4 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column5 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column6 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column7 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        CType(Me.dataGridview1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'dataGridview1
+        '
+        Me.dataGridview1.AllowUserToAddRows = False
+        Me.dataGridview1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dataGridview1.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.Column1, Me.Column2, Me.Column3, Me.Column4, Me.Column5, Me.Column6, Me.Column7})
+        Me.dataGridview1.Location = New System.Drawing.Point(21, 138)
+        Me.dataGridview1.Name = "dataGridview1"
+        Me.dataGridview1.Size = New System.Drawing.Size(759, 205)
+        Me.dataGridview1.TabIndex = 3
+        '
+        'btnImport
+        '
+        Me.btnImport.Location = New System.Drawing.Point(21, 89)
+        Me.btnImport.Name = "btnImport"
+        Me.btnImport.Size = New System.Drawing.Size(94, 31)
+        Me.btnImport.TabIndex = 4
+        Me.btnImport.Text = "Import"
+        Me.btnImport.UseVisualStyleBackColor = True
+        '
+        'txtpathfile
+        '
+        Me.txtpathfile.Location = New System.Drawing.Point(21, 39)
+        Me.txtpathfile.Name = "txtpathfile"
+        Me.txtpathfile.Size = New System.Drawing.Size(323, 20)
+        Me.txtpathfile.TabIndex = 5
+        '
+        'Column1
+        '
+        Me.Column1.DataPropertyName = "StartDate"
+        Me.Column1.HeaderText = "StartDate"
+        Me.Column1.Name = "Column1"
+        '
+        'Column2
+        '
+        Me.Column2.DataPropertyName = "End Date"
+        Me.Column2.HeaderText = "End Date"
+        Me.Column2.Name = "Column2"
+        '
+        'Column3
+        '
+        Me.Column3.DataPropertyName = "UserCD"
+        Me.Column3.HeaderText = "UserCD"
+        Me.Column3.Name = "Column3"
+        '
+        'Column4
+        '
+        Me.Column4.DataPropertyName = "User Name"
+        Me.Column4.HeaderText = "User Name"
+        Me.Column4.Name = "Column4"
+        '
+        'Column5
+        '
+        Me.Column5.DataPropertyName = "Full Name"
+        Me.Column5.HeaderText = "Full Name"
+        Me.Column5.Name = "Column5"
+        '
+        'Column6
+        '
+        Me.Column6.DataPropertyName = "Address"
+        Me.Column6.HeaderText = "Address"
+        Me.Column6.Name = "Column6"
+        '
+        'Column7
+        '
+        Me.Column7.DataPropertyName = "Remarks"
+        Me.Column7.HeaderText = "Remarks"
+        Me.Column7.Name = "Column7"
+        '
+        'CSV_Import
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(853, 401)
+        Me.Controls.Add(Me.txtpathfile)
+        Me.Controls.Add(Me.btnImport)
+        Me.Controls.Add(Me.dataGridview1)
+        Me.Name = "CSV_Import"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "CSV_Import"
+        CType(Me.dataGridview1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents dataGridview1 As DataGridView
+    Friend WithEvents btnImport As Button
+    Friend WithEvents txtpathfile As TextBox
+    Friend WithEvents Column1 As DataGridViewTextBoxColumn
+    Friend WithEvents Column2 As DataGridViewTextBoxColumn
+    Friend WithEvents Column3 As DataGridViewTextBoxColumn
+    Friend WithEvents Column4 As DataGridViewTextBoxColumn
+    Friend WithEvents Column5 As DataGridViewTextBoxColumn
+    Friend WithEvents Column6 As DataGridViewTextBoxColumn
+    Friend WithEvents Column7 As DataGridViewTextBoxColumn
+End Class
