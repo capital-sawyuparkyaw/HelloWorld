c1ac99b9299eedc7d07c618f8c6b6c3a28c10612
Shwe Eain San <Shwe Eain San@DESKTOP-73GSVF4>
2022-06-20 13:47:17 +0630

    by ses

---
+++ TextFile_Test/Form1.vb
@@ -0,0 +1,20 @@
+﻿Imports System
+Imports System.IO
+Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim folderPath As String
+        Dim lines() As String
+        Dim vals() As String
+        folderPath = "C:\Users\Shwe Eain San\Downloads\textfile.txt"
+
+        lines = File.ReadAllLines(folderPath)
+        For i As Integer = 0 To lines.Length - 1 Step +1
+            vals = lines(i).ToString().Split(" ")
+            Dim row(vals.Length - 1) As String
+            For j As Integer = 0 To vals.Length - 1 Step +1
+                row(j) = vals(j).Trim
+            Next j
+            DataGridView1.Rows.Add(row)
+        Next i
+    End Sub
+End Class
